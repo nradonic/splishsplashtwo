3a57e6bc9571a0b17054b94e158a542975a2968c
Nick Radonic <big.rad@gmail.com>
2015-12-17 12:25:14 -0500

    Splash now propagates from a single point outward - with cell aging...

--- drawHexagons.js
+++ drawHexagons.js
@@ -1,5 +1,5 @@
 var numrows = 9;
-var numcols = 30;
+var numcols = 9;
 var radius = 30;
 var baseX = 10;
 var baseY = 10;
@@ -8,7 +8,7 @@ var newDropX = 10;
 var newDropY = 10;
 var myVar;
 
-var splashDelay = 4;
+var splashDelay = 10;
 var rippleDelay = 500;
 var timeCounter = 0;
 
@@ -20,7 +20,15 @@ var ledStateJustTurnedOn = 2;
         //0    x x         2 1
         //1   x x x       3 0 0
         //2    x x         4 5
-        
+var cellr=0;
+var cellur=1;
+var cellul=2;
+var celll=3;
+var cellll=4;
+var celllr=5;
+var neighborIsTurnedOn = 1;
+var neighborIsTurnedOff = 0;
+      
 function left1(X){return X-1;}
 function right1(X){return X+1;}
 function updownLeft(Y,X){
@@ -37,14 +45,6 @@ initial = function(){
 	var temp = {valL : 0, dir : [0,0,0,0,0,0]}; // dir={cellr=0, cellur=1, cellul=2, celll=3, cellll=4, celllr=5}
 	return temp;
 }
-var cellr=0;
-var cellur=1;
-var cellul=2;
-var celll=3;
-var cellll=4;
-var celllr=5;
-var neighborTurnedOn = 1;
-var neighborTurnedOff = 0;
 
 // extension to Array type for 2D, with initialization - from Douglas Crockford
 Array.matrix = function(numrows, numcols, initial){
@@ -63,9 +63,10 @@ var ledArray = Array.matrix(numrows,numcols, initial); // create the data matrix
 var ledArrayTemp = Array.matrix(numrows,numcols, initial); // create the data matrix, fill with zeros
 var ledsTemp;
 
-function drawHex(c2, radius, xOffset0, yOffset0, color) {
+function drawHex(c2, radius, xOffset0, yOffset0, color, dir) {
 
     c2.fillStyle = color;
+    // radius0  is 1 pixel smaller than radius value - buffer for line
 		var radius0 = radius - 1;
     var radiusCos30 = radius0 * Math.sqrt(3) / 2;
     var radiusSin30 = radius0 * 0.5;
@@ -86,36 +87,79 @@ function drawHex(c2, radius, xOffset0, yOffset0, color) {
     c2.closePath();
     c2.stroke();
     c2.fill();
+    // draw direction arrows
+    c2.beginPath();
+    c2.strokeStyle = "#FF0000";
+	if(dir[cellr]!=0){
+	    c2.moveTo(radiusCos30 + xOffset0, yOffset0);
+	    c2.lineTo(xOffset0, yOffset0);
+	}
+	if(dir[cellur]!=0){
+	    c2.moveTo(radiusCos60 + xOffset0, -radiusSin60 + yOffset0);
+	    c2.lineTo(xOffset0, yOffset0);
+	}
+	if(dir[cellul]!=0){
+	    c2.moveTo(-radiusCos60 + xOffset0, -radiusSin60 + yOffset0);
+	    c2.lineTo(xOffset0, yOffset0);
+	}
+	if(dir[celll]!=0){
+	    c2.moveTo(-radiusCos30 + xOffset0, yOffset0);
+	    c2.lineTo(xOffset0, yOffset0);
+	}
+	if(dir[cellll]!=0){
+	   c2.moveTo(-radiusCos60 + xOffset0, radiusSin60 + yOffset0);
+	   c2.lineTo(xOffset0, yOffset0);
+	}
+	if(dir[celllr]!=0){
+	    c2.moveTo(radiusCos60 + xOffset0, radiusSin60 + yOffset0);
+	    c2.lineTo(xOffset0, yOffset0);
+	}
+	c2.closePath();
+	c2.stroke();
 }
 
 // color constants
 var green = "#00FF00";
-var black = "#000000";
+var yellow = "#C0C000";
+var black = "#808080";
 	
 function getRandomColor(){
 	if( Math.floor(Math.random() * 1.1 ) === 1) {return green;}
 	else {return black;} 
 }
 
+// Even returns 0, Odd returns 1
+function evenOdd(x){return x%2;}
+var even = 0;
+var odd = 1;
+
 function drawHexagons(ctx, leds){
 	var printIt = document.getElementById("printIt");
 
-	var r3 = Math.sqrt(3) / 2;
-	var yStep = 3 * radius;
+	var r3 = Math.sqrt(3);
+	var yStep = 1.5 * radius;
 	var xStep = r3 * radius;
-	var toggle = 1.5 * radius;
+	var toggle = r3/2 * radius;
 
 	
 	for (var row = 0; row < numrows; row += 1) {
    	  for (var col = 0; col < numcols; col += 1) {
-        var xPos = col * xStep + baseX;
+        var toggleX = ((row+1) % 2) * toggle;
+        var xPos = col * xStep + baseX + toggleX;
         var yPos = row * yStep + baseY;
-        // right on even columns, left on odd
-        //0    x x
-        //1   x x x
-        //2    x x
-        var toggle2 = toggle * ((1+col) % 2);
-        drawHex(ctx, radius, xPos, yPos + toggle2, leds[row][col].valL===0?black:green);
+        var ledColor = black;
+        switch (leds[row][col].valL){
+          	case ledStateOff: 
+          					ledColor = black;
+          					break;
+            case ledStateStale: 
+            				ledColor = yellow;
+            				break;
+            case ledStateJustTurnedOn:
+            				ledColor = green;
+            				break;
+        }
+        drawHex(ctx, radius, xPos, yPos, ledColor, leds[row][col].dir);
         // printIt.innerHTML = " " + radius + " " + xStep + " " + yStep + " " + row + " " + col+ " " + toggle2 + " " + toggle + " ";
     	}
 	}
@@ -139,32 +183,51 @@ function checkTags(leds, Y, X){
 function setNeighborDirection(leds, Y,X){
 	// tag left
 	if(X>0){
-		leds[Y][X-1].dir[cellr]=neighborTurnedOn;
+		if(leds[Y][X].dir[cellr]===neighborIsTurnedOff 
+			&& leds[Y][X-1].valL===ledStateOff){
+			leds[Y][X-1].dir[cellr]=neighborIsTurnedOn;
+		}
 	}
 	// tag right
 	if(X<numcols-1){
-		leds[Y][X+1].dir[celll]=neighborTurnedOn;
+		if(leds[Y][X].dir[celll]===neighborIsTurnedOff 
+			&& leds[Y][X+1].valL===ledStateOff){
+			leds[Y][X+1].dir[celll]=neighborIsTurnedOn;
+		}
 	}
 	// tag upright
-	if(Y>0 && X<numcols-1){
-		leds[Y-1][updownRight(Y,X)].dir[cellll]=neighborTurnedOn;
+	if(Y>0 && (X<numcols-1 || evenOdd(Y)===odd)){
+		if(leds[Y][X].dir[cellll]===neighborIsTurnedOff 
+			&& leds[Y-1][updownRight(Y,X)].valL===ledStateOff){
+			leds[Y-1][updownRight(Y,X)].dir[cellll]=neighborIsTurnedOn;
+		}
 	}
 	// tag downright
-	if(Y<numrows-1 && X<numcols-1){
-		leds[Y+1][updownRight(Y,X)].dir[cellul]=neighborTurnedOn;
+	if(Y<numrows-1 && (X<numcols-1 || evenOdd(Y)===odd)){
+		if(leds[Y][X].dir[cellul]===neighborIsTurnedOff 
+			&& leds[Y+1][updownRight(Y,X)].valL===ledStateOff){
+			leds[Y+1][updownRight(Y,X)].dir[cellul]=neighborIsTurnedOn;
+		}
 	}
 	// tag upleft
-	if(Y>0 && X>0){
-		leds[Y-1][updownLeft(Y,X)].dir[celllr]=neighborTurnedOn;
+	if(Y>0 && (X>0 || evenOdd(Y)===even)) {
+		if(leds[Y][X].dir[celllr]===neighborIsTurnedOff 
+			&& leds[Y-1][updownLeft(Y,X)].valL===ledStateOff){
+			leds[Y-1][updownLeft(Y,X)].dir[celllr]=neighborIsTurnedOn;
+		}
 	}
 	// tag downleft
-	if(Y<numrows-1 && X<numcols-1){
-		leds[Y+1][updownLeft(Y,X)].dir[cellur]=neighborTurnedOn;
+	if(Y<numrows-1 && (X>0 || evenOdd(Y)===even)){
+			if(leds[Y][X].dir[cellur]===neighborIsTurnedOff 
+			&& leds[Y+1][updownLeft(Y,X)].valL===ledStateOff){
+				leds[Y+1][updownLeft(Y,X)].dir[cellur]=neighborIsTurnedOn;
+			}
 	}
 }
 
 // propagate changes to next generation
 function calculateRipples(ledsOld, ledsNext){
+	// age light states
 	for (nrows=0; nrows<numrows; nrows++){
 		for (ncols=0; ncols<numcols; ncols++){
 			if(ledsOld[nrows][ncols].valL === ledStateOff){
@@ -172,22 +235,33 @@ function calculateRipples(ledsOld, ledsNext){
 			}
 			if(ledsOld[nrows][ncols].valL === ledStateJustTurnedOn){
 				ledsNext[nrows][ncols].valL = ledStateStale;
+				
 			}
 			if(ledsOld[nrows][ncols].valL === ledStateStale){
 				ledsNext[nrows][ncols].valL = ledStateOff;
 			}
 		}
 	}
+	// propagate the changes look for change tags and turn on those lights
 	for (nrows=0; nrows<numrows; nrows++){
 		for (ncols=0; ncols<numcols; ncols++){
 			if(checkTags(ledsOld, nrows, ncols) > 0) {
 				ledsNext[nrows][ncols].valL = ledStateJustTurnedOn;
 				clearTags(ledsNext, nrows, ncols);
 				clearTags(ledsOld, nrows, ncols);
+			}
+		}
+	}
+	// set neighbor change tags
+	for (nrows=0; nrows<numrows; nrows++){
+		for (ncols=0; ncols<numcols; ncols++){
+			if(ledsNext[nrows][ncols].valL === ledStateJustTurnedOn){
+				setNeighborDirection(ledsNext, nrows, ncols);
 				
 			}
 		}
 	}
+
 }
 
 
